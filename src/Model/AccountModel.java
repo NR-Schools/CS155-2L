6629fc2a776253e2ac14fd3cf57aee02cef50f70
Null-Root <triascend94@gmail.com>
2023-03-02 16:29:28 +0800

    Create AccountModel.java

---
+++ src/Model/AccountModel.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author hp
+ */
+public class AccountModel {
+    private String Username;
+    private String Password;
+    private String ContactNumber;
+    private String EmailAddress;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public String getContactNumber() {
+        return ContactNumber;
+    }
+
+    public void setContactNumber(String ContactNumber) {
+        this.ContactNumber = ContactNumber;
+    }
+
+    public String getEmailAddress() {
+        return EmailAddress;
+    }
+
+    public void setEmailAddress(String EmailAddress) {
+        this.EmailAddress = EmailAddress;
+    }
+    
+    
+}
